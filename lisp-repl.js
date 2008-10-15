862f0e251f04fcd34700b7e96f5edf9838268f3d
Manuel Simoni <msimoni@gmail.com>
2008-10-15 02:50:10 +0200

    init

--- lisp-repl.js
+++ lisp-repl.js
@@ -3,14 +3,16 @@ load("lisp-decoder.js");
 load("lisp-compiler.js");
 load("lisp-emitter.js");
 
+print("CyberLisp 0.0.1");
+
 while(1) {
     try {
         var lispText = readline();
         var lispForms = lispParse(lispText);
         var lispIR = lispDecode(lispForms[0]);
         var lispJR = lispCompile(lispIR);
-        var lispJavaScriptText = lispEmit(lispJR);
-        print(eval(lispJavaScriptText));
+        var lispJS = lispEmit(lispJR);
+        print(eval(lispJS));
     } catch(e) {
         print(e);
     }
