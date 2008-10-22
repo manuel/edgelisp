84560054f4ab7d61882795f80e2bfe2009bb9399
Manuel Simoni <msimoni@gmail.com>
2008-10-22 12:59:09 +0200

    misc stuff

--- std-lib/defcaseclass.lisp
+++ std-lib/defcaseclass.lisp
@@ -12,14 +12,3 @@
      ,@(map (lambda (c) 
               `(defclass (,(first c) ,name) ,@(rest c)))
             cases)))
-
-(defmacro defcaseclass (name @cases)
-  (--append (--compound (--symbol "progn")
-                        (--compound (--symbol "defclass")
-                                    name))
-            (map (lambda (c)
-                   (--append (--compound (--symbol "defclass")
-                                         (--compound (first c) name))
-                             (rest c)))
-                 cases)))
-
