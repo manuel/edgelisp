41b11fe28f49f9fd28cb655cfab2fdbaa1e32797
Manuel Simoni <msimoni@gmail.com>
2011-04-16 14:46:59 +0200

    primitive REPL

--- lisp.js
+++ lisp.js
@@ -17,8 +17,8 @@
    Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
    Boston, MA 02110-1301, USA. */
 
-load("lib/json2.js");
-load("lib/jsparse.js");
+//load("lib/json2.js");
+//load("lib/jsparse.js");
 
 
 /*** Syntax ***/
