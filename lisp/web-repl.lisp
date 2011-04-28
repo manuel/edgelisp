bd3c12a7f52164960bdbe768ea1bb8c5785e1c20
Manuel Simoni <msimoni@gmail.com>
2011-04-28 15:07:12 +0200

    abort

--- lisp/web-repl.lisp
+++ lisp/web-repl.lisp
@@ -1,6 +1,5 @@
 (defun web-repl-eval (form)
-  (block toplevel
-    (handler-bind ((return-to-toplevel
-                    (lambda (r) (return-from toplevel))))
+  (block abort
+    (handler-bind ((abort
+                    (lambda (r) (return-from abort))))
       (eval form))))
-
