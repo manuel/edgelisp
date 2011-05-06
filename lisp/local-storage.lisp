ddd8733dd5a47552c22b921bb83db670abf6b161
Manuel Simoni <msimoni@gmail.com>
2011-05-06 13:32:53 +0200

    local-storage-{get,set}-item

--- lisp/local-storage.lisp
+++ lisp/local-storage.lisp
@@ -1,3 +1,5 @@
+(defclass local-storage-error (error))
+
 (defun local-storage-supported? ()
   (native-body #{
     try {
@@ -6,5 +8,15 @@
       return false;
     }
     #}))
+
+(defun local-storage-get-item ((key string))
+  (unless (local-storage-supported?)
+    (error (make local-storage-error)))
+  #{ localStorage.getItem((~key)) #})
+
+(defun local-storage-set-item ((key string) (value string))
+  (unless (local-storage-supported?)
+    (error (make local-storage-error)))
+  #{ localStorage.setItem((~key), (~value)) #})
 
 (provide "local-storage")
