e633206b0d46e5470f067aea0b14ff982594b7ae
Manuel Simoni <msimoni@gmail.com>
2011-05-07 17:41:16 +0200

    google-feed-load

--- client/google-feed.lisp
+++ client/google-feed.lisp
@@ -1,9 +1,4 @@
-(defdynamic google-feed-api-key
-  "ABQIAAAAsDylyHuLqoMVDQ-FI1o2rRQIuhHBNtSzOwBJ-bNgzgP7yqq0KBSIT9w56_5MKceXF5GR3HiVt6h9uQ")
-
-(defun google-feed-api-script-url ()
-  (string-concat "https://www.google.com/jsapi?key=" (dynamic google-feed-api-key)))
-
-(dom-load-script (google-feed-api-script-url))
+(defun google-feed-load ((url string) (callback function))
+  #{ new google.feeds.Feed(~url).load(~(native-callback callback)) #})
 
 (provide "google-feed")
