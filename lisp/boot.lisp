b17d920efc864dd9f5eecc33b44d5ab1d2ee6dbf
Manuel Simoni <msimoni@gmail.com>
2011-05-07 16:42:48 +0200

    print note at beginning of boot

--- lisp/boot.lisp
+++ lisp/boot.lisp
@@ -13,6 +13,8 @@
 ; You should have received a copy of the GNU Affero General Public License
 ; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+(%%eval-when-compile (%note "Lisp boot"))
+
 ;;;; Bootstrap defmacro
 
 (%%defsyntax defmacro
