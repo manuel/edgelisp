bc73ee5c042f647cd1720ce8f347df24713f6651
Manuel Simoni <msimoni@gmail.com>
2011-05-16 20:09:47 +0200

    early local storage for content tracker

--- client/base.lisp
+++ client/base.lisp
@@ -1,18 +1,21 @@
-;;;; Data Model
+;;;; Content Tracker, modelled after Git
 
-(defclass base:blob ()
-  (media-type
-   content-encoding
-   body))
+(defclass base:object)
 
-(defclass base:tree ()
+(defclass base:blob (base:object)
+  (body))
+
+(defclass base:tree (base:object)
   (dentries))
+
+(defun base:make-tree ()
+  (make base:tree :dentries (list)))
 
 (defclass base:dentry ()
   (name
    id))
 
-(defclass base:commit ()
+(defclass base:commit (base:object)
   (tree-id))
 
 (defclass base:ref ()
@@ -20,44 +23,69 @@
 
 (defclass base:repo ()
   (name
-   heads))
+   tree-id))
 
-(defun base:make-repo ((name string))
-  (make base:repo :name name :heads (list)))
+(defun base:make-repo ((name string) (tree-id string) -> base:repo)
+  (make base:repo :name name :tree-id tree-id))
 
-(defmethod show-object ((r base:repo))
+(defmethod show-object ((r base:repo) -> string)
   (.name r))
+
+;;;; Storage Providers
+
+(defclass base:provider)
+
+(defgeneric base:open-repo (provider name -> repo))
+
+;;;; Local Storage Provider
+
+;;; The local storage provider stores repositories very inefficiently
+;;; using HTML5 localStorage which is widely supported.
+
+(defclass base:local-provider (base:provider)
+  (prefix))
+
+(defun base:local-key ((p base:local-provider) (repo-name string) -> string)
+  "Returns local storage key for repository name."
+  (string-concat (.prefix p) ".repo." repo-name))
+
+(defmethod base:open-repo ((p base:local-provider) (name string) -> base:repo)
+  (let* ((key (base:local-key p name))
+         (tree-id (local-storage-get-item key)))
+    (if (nil? tree-id)
+        (base:local-init-repo p name)
+        (base:make-repo name tree-id))))
+
+(defun base:local-init-repo ((p base:local-provider) (name string) -> base:repo)
+  (let* ((tree (base:make-tree))
+         (tree-id (base:store p tree)))
+    (base:make-repo name tree-id)))
 
 ;;;; API
 
-(defun base:list-repos (-> list)
-  "Returns locally stored repositories."
-  (list))
+(defun base:hash ((s string) -> string)
+  (sha1 s))
 
-;;;; User Interface
+(defun base:store ((p base:local-provider) (obj base:object) -> string)
+  "Stores object and returns its ID."
+  (let* ((content (list (base:canonical-type obj) (base:canonical-content obj)))
+         (json-content (to-json content))
+         (id (base:hash json-content)))
+    (local-storage-set-item id json-content)
+    id))
 
-(defclass base:tracker-mode (mode))
+(defgeneric base:canonical-content (base-object -> object)
+  (:documentation "Translates a base object to JSON."))
+(defmethod base:canonical-content ((b base:blob))
+  (.body b))
+(defmethod base:canonical-content ((t base:tree))
+  (apply \list (map \base:canonical-content (.dentries t))))
+(defmethod base:canonical-content ((c base:commit))
+  (.tree-id c))
 
-(defun base:make-tracker-mode (-> base:tracker-mode)
-  (make base:tracker-mode))
+(defgeneric base:canonical-type (base-object -> string))
+(defmethod base:canonical-type ((b base:blob)) "blob")
+(defmethod base:canonical-type ((t base:tree)) "tree")
+(defmethod base:canonical-type ((c base:commit)) "commit")
 
-(defmethod mode-init-window ((m base:tracker-mode) (w window))
-  (set-window-title w "Content Tracker")
-  (dom-append-child
-   (window-element w)
-   (<div>
-    (<div> (<strong> "Repositories"))
-    (base:markup-repos))))
-
-(defun base:markup-repos (-> native)
-  (let ((repos (base:list-repos)))
-    (if (empty? repos)
-        (<em> "none")
-        (apply \<ul> (map (lambda (repo)
-                            (<li> (markup repo)))
-                          repos)))))
-
-(defdynamic base:tracker-window (make-window (dynamic window-manager)
-                                             nil
-                                             :mode (base:make-tracker-mode)))
-
+(provide "base")
