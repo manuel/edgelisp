205f5a935b938f248d47d6809ac39a43d09199c4
Manuel Simoni <msimoni@gmail.com>
2011-05-26 15:22:46 +0200

    added bootstrapper's lament

--- lisp/boot.lisp
+++ lisp/boot.lisp
@@ -13,6 +13,8 @@
 ; You should have received a copy of the GNU Affero General Public License
 ; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+;;; ``An adequate bootstrap is a contradiction in terms.'' -- Alan Perlis
+
 ;;;; Bootstrap defmacro
 
 (%%defsyntax defmacro
